fa21f01f2934c3adcf1f367c4d56d18616333dcd
FranciscoGontijo <franciscoacmg@gmail.com>
2023-01-29 16:56:22 +1100

    Notifications sounds

--- src/components/timer/timer.js
+++ src/components/timer/timer.js
@@ -3,6 +3,9 @@ import { useSelector, useDispatch } from "react-redux";
 import { selectSettings, reduceRound, resetRound } from "../timersettings/settingsslice";
 
 import "./timer.css";
+import toShortBreakSound from "../../assets/toShortBreakSound.wav";
+import toLongBreakSound from "../../assets/toLongBreakSound.wav";
+import toFocusSound from "../../assets/toFocusSound.wav";
 
 import FocusTimer from "./focus";
 import ShortBreakTimer from "./shortbreak";
@@ -17,20 +20,35 @@ const Timer = () => {
 
     useEffect(() => { setSessions(sessions => sessions = settings.rounds)}, []);
 
+    const playShort = () => {
+        new Audio(toShortBreakSound).play();
+    }
+
+    const playFocus = () => {
+        new Audio(toFocusSound).play();
+    }
+
+    const playLong = () => {
+        new Audio(toLongBreakSound).play();
+    }
+
     const changeTimer = () => {
         //change to back to focus
         if (timer === 'short' || timer === 'long') {
             setTimer(time => time = 'focus');
+            playFocus();
             toggleStart();
         }
         //change to short break
         if (settings.rounds > 1 && timer === 'focus') {
             setTimer(time => time = 'short');
+            playShort();
             toggleStart();
         }
         //change to long break
         if (settings.rounds === 1 && timer === 'focus') {
             setTimer(time => time = 'long');
+            playLong();
             toggleStart();
         }
     };
@@ -39,15 +57,18 @@ const Timer = () => {
         if (timer === 'short' || timer === 'long') {
             //change to focus
             setTimer(time => time = 'focus');
+            playFocus();
         }
         if (settings.rounds > 1 && timer === 'focus') {
             //change to short break
             setTimer(time => time = 'short');
+            playShort();
             dispatch(reduceRound());
         }
         if (settings.rounds === 1 && timer === 'focus') {
             //change to long break
             setTimer(time => time = 'long');
+            playLong();
             dispatch(resetRound());
         }
     };
