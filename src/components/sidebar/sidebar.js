ed31af16ef0b68e1d23327bd77ef3ebe24206ae8
FranciscoGontijo <franciscoacmg@gmail.com>
2023-01-30 14:03:07 +1100

    Using Status to change CSS and Disable Links on SideBar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,15 +1,25 @@
-import "./sidebar.css"
 import { Link } from "react-router-dom"
+import { useSelector } from "react-redux";
+import { selectTimer } from "../timer/timerslice";
+
+import "./sidebar.css"
 
     
 const SideBar = () => {
+    const { status } = useSelector(selectTimer);
     return (
         <nav>
             <img src="#" alt="logo" />  
             <ul>
                 <li><Link to="/timer"><i class="fa-regular fa-clock"></i>Timer</Link></li>
-                <li><Link to="/stats"><i class="fa-solid fa-chart-column"></i>Stats</Link></li>
-                <li><Link to="/settings"><i class="fa-solid fa-gear"></i>Settings</Link></li>
+                <li><Link
+                    style={status !== "ON_HOLD" ? {pointerEvents: "none"} : null}
+                    className={status === "ON_HOLD" ? 'functioning' : 'disabled'} 
+                    to="/stats"><i class="fa-solid fa-chart-column"></i>Stats</Link></li>
+                <li><Link 
+                    style={status !== "ON_HOLD" ? {pointerEvents: "none"} : null}
+                    className={status === "ON_HOLD" ? 'functioning' : 'disabled'} 
+                    to="/settings"><i class="fa-solid fa-gear" ></i>Settings</Link></li>  
                 <li><i class="fa-solid fa-moon"></i>Dark Mode</li>
             </ul>
         </nav>
