79a23f56634e120e6adefc5f1ca89132ee6bea8f
FranciscoGontijo <franciscoacmg@gmail.com>
2023-02-20 23:13:49 +1100

    Refactoring

--- src/components/labeltag/labellist.js
+++ src/components/labeltag/labellist.js
@@ -10,17 +10,12 @@ const LabelList = ({handleCreate, handleSelect, closeDisplay, deleteLabel}) => {
     const labelList = useSelector(selectLabelList);
     const label = useSelector(selectLabel);
 
-    //Icons functionality
-    //Create edit label
-    //Create delete label
-
     return (
         <div className="label-list-container">
             <span>Labels</span><i class="fa-solid fa-xmark x-mark" onClick={closeDisplay}></i>
             <ul>
-            {labelList.map((label, index)=> {
-                let color = label.color
-                return <li id={index} onClick={handleSelect}><span style={{backgroundColor: color}} className="color-display"></span>{label.label}<i onClick={deleteLabel} class="fa-solid fa-trash"></i></li>
+            {labelList.map(({ label, color }, index)=> {
+                return <li id={index} onClick={handleSelect}><span style={{backgroundColor: color}} className="color-display"></span>{label}<i onClick={deleteLabel} class="fa-solid fa-trash"></i></li>
             })}
             <li className="add-button" onClick={handleCreate}><i class="fa-solid fa-plus"></i><span className="add-button-text">Add new Label</span></li>
             </ul>
