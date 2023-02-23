8348443fe5daab0a5fd9031d68058b73204e8089
FranciscoGontijo <franciscoacmg@gmail.com>
2023-02-23 21:23:15 +1100

    Still organizing

---
+++ src/slices/userSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState: {
+        user: {
+            userEmail: ''
+        }
+    },
+    reducers: {
+        createUser: (state, action) => {
+            state.user.userEmail = action.payload;
+        }
+    }
+});
+
+export const { createUser } = userSlice.actions;
+export const selectUser = (state) => state.user.user;
+export default userSlice.reducer;
