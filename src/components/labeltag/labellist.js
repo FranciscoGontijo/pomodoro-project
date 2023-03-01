8654d8bbad1d065ee3ebecbe59f0ff945ab0e003
FranciscoGontijo <franciscoacmg@gmail.com>
2023-03-01 16:39:42 +1100

    Accepting incoming changes

--- src/components/labeltag/labellist.js
+++ src/components/labeltag/labellist.js
@@ -1,12 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-import { useSelector } from "react-redux";
-import { selectLabelList } from "./labeltagslice";
 
-import './labellist.css'
-
-const LabelList = ({ openForm, selectLabel, closeDisplay, deleteLabel }) => {
-=======
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { selectLabelList, deleteLabel, changeLabel } from "../../slices/labeltagslice";
@@ -16,7 +9,6 @@ import UserPool from "../../util/UserPool";
 import './labellist.css'
 
 const LabelList = ({ goToForm, closeDisplay }) => {
->>>>>>> newBranchTest
     const labelList = useSelector(selectLabelList);
     const { userEmail } = useSelector(selectUser);
     const dispatch = useDispatch();
@@ -63,16 +55,6 @@ const LabelList = ({ goToForm, closeDisplay }) => {
             <ul>
                 {labelList.map(({ label, color }, index) => {
                     return <div className="list-div">
-<<<<<<< HEAD
-                        <li id={index} onClick={selectLabel}>
-                            <span style={{ backgroundColor: color }} className="color-display"></span>
-                            {label}
-                        </li>
-                        <i id={index} onClick={deleteLabel} class="fa-solid fa-trash"></i>
-                    </div>
-                })}
-                <li className="add-button" onClick={openForm}><i class="fa-solid fa-plus"></i><span className="add-button-text">Add new Label</span></li>
-=======
                         <li key={index} id={label} onClick={selectLabel}>
                             <span style={{ backgroundColor: color }} className="color-display"></span>
                             {label}
@@ -81,7 +63,6 @@ const LabelList = ({ goToForm, closeDisplay }) => {
                     </div>
                 })}
                 <li className="add-button" onClick={goToForm}><i class="fa-solid fa-plus"></i><span className="add-button-text">Add new Label</span></li>
->>>>>>> newBranchTest
             </ul>
         </div>
     )
--- src/components/labeltag/labeltag.js
+++ src/components/labeltag/labeltag.js
@@ -1,11 +1,6 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
-import { useSelector, useDispatch } from "react-redux";
-import { selectCurrentLabel, selectLabelList, changeLabel, deleteLabel } from './labeltagslice';
-=======
 import { useSelector } from "react-redux";
 import { selectCurrentLabel, selectLabelList } from '../../slices/labeltagslice';
->>>>>>> newBranchTest
 import "./labeltag.css"
 
 import LabelForm from './labelform';
@@ -20,12 +15,7 @@ const LabelTag = () => {
     const [displayForm, setDisplayForm] = useState(false);
     const [displayList, setDisplayList] = useState(false);
 
-<<<<<<< HEAD
-    //Open Label List or Label Form
-    const openLabelList = () => {
-=======
     const openDisplay = () => {
->>>>>>> newBranchTest
         if (labelList.length > 0) {
             setDisplayList(displayList ? false : true);
         } else {
@@ -39,52 +29,11 @@ const LabelTag = () => {
     }
 
     //Show LabelForm instead of LabelList
-<<<<<<< HEAD
-    const openLabelForm = () => {
-=======
     const goToForm = () => {
->>>>>>> newBranchTest
         setDisplayForm(true);
         setDisplayList(false);
     };
 
-<<<<<<< HEAD
-    //Select label to be the current one
-    const selectLabel = (e) => {
-        e.preventDefault();
-        const index = e.target.id;
-        const labelSelected = labelList[index];
-        dispatch(changeLabel(labelSelected));
-        setDisplayList(false);
-    };
-
-    //Delete label from the label list using icon
-    const handleDelete = (e) => {
-        e.preventDefault();
-        const index = e.target.id;
-        dispatch(deleteLabel(index));
-        if (labelList.length <= 1) {
-            dispatch(changeLabel({
-                label: 'ADD LABEL',
-                color: 'rgb(104, 85, 224)'
-            }));
-            setDisplayList(false);
-        }
-    };
-
-    return (
-        <div className="labeltag">
-            <button onClick={openLabelList} style={{color: color}} className={"label-button"}>
-                <i className={"fa-solid fa-tag " + color}></i> {label}
-            </button>
-            { displayForm && <LabelForm closeDisplay={closeDisplay}/>}
-            { displayList && 
-            <LabelList 
-                deleteLabel={handleDelete}
-                closeDisplay={closeDisplay}
-                openForm={openLabelForm}
-                selectLabel={selectLabel}/>}
-=======
     return (
         <div className="labeltag">
             <button onClick={openDisplay} style={{ color: color }} className={"label-button"}>
@@ -96,7 +45,6 @@ const LabelTag = () => {
                     closeDisplay={closeDisplay}
                     goToForm={goToForm}
                 />}
->>>>>>> newBranchTest
         </div>
     )
 };
