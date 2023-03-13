05b5377b89b02ae689e3b5640d666b0b2f962d37
FranciscoGontijo <franciscoacmg@gmail.com>
2023-03-13 14:25:45 +1100

    CSS and title of disabled nav-links

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -4,22 +4,26 @@ import { selectTimer } from "../../slices/timerslice";
 
 import "./sidebar.css"
 
-    
+
 const SideBar = () => {
     const { status } = useSelector(selectTimer);
     return (
         <nav className="sidebar-nav">
-            <img className="logo" src="#" alt="logo" />  
+            <img className="logo" src="#" alt="logo" />
             <ul>
-                <li><Link className="link" to="/timer"><i class="fa-regular fa-clock"></i>Timer</Link></li>
-                <li><Link
-                    style={status !== "ON_HOLD" ? {pointerEvents: "none"} : null}
-                    className={status === "ON_HOLD" ? 'functioning link' : 'link disabled'} 
-                    to="/stats"><i class="fa-solid fa-chart-column"></i>Stats</Link></li>
-                <li><Link 
-                    style={status !== "ON_HOLD" ? {pointerEvents: "none"} : null}
-                    className={status === "ON_HOLD" ? 'link functioning' : 'link disabled'} 
-                    to="/settings"><i class="fa-solid fa-gear" ></i>Settings</Link></li>  
+                <li className="functioning-nav-link"><Link className="link" to="/timer"><i class="fa-regular fa-clock"></i>Timer</Link></li>
+                <li className={status === "ON_HOLD" ? 'functioning-nav-link link' : 'link disabled-nav-link'}
+                    title={status !== "ON_HOLD" && 'Reset timer to access Stats'}>
+                    <Link
+                        style={status !== "ON_HOLD" ? { pointerEvents: "none" } : null}
+                        className="link"
+                        to="/stats"><i class="fa-solid fa-chart-column"></i>Stats</Link></li>
+                <li className={status === "ON_HOLD" ? 'functioning-nav-link link' : 'link disabled-nav-link'}
+                    title={status !== "ON_HOLD" && 'Reset timer to access Settings'}>
+                    <Link
+                        style={status !== "ON_HOLD" ? { pointerEvents: "none" } : null}
+                        className="link"
+                        to="/settings"><i class="fa-solid fa-gear" ></i>Settings</Link></li>
             </ul>
         </nav>
     )
